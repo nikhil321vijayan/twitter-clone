2590fdad586bc4748c47912d84e3eb29f3b8e869
Nikhil Vijayan <nikhil321vijayan@gmail.com>
2022-12-02 17:08:49 -0500

    Added Sidebar component into the app
    * Added Material UI(outdated) core and icons packages to node.
    * Added Sidebar component to the App.js
    * Added TwitterIcon to the Sidebar component.
    * Added SidebarOption component(each side bar option)
    * Passed Material UI Icons as props in Sidebar component to the SidebarOption component.
    * Added flex css to the sidebarOption.css file.
    * Added --twitter-color and --twitter-background-color to App.css as global variables.
    * Used the --twitter-color and --twitter-background-color variables in SidebarOptions.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     // BEM
     <div className="app">
       <h1>Twitter clone</h1>
+
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
