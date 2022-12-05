b8c5e2676965f513a2e245b518fd36741ec2112b
Nikhil Vijayan <nikhil321vijayan@gmail.com>
2022-12-05 15:13:10 -0500

    Added Feed and Widgets components
    * Added flex as the display in App.css and set the max width to 1300px.
    * App.css => Set margin-left and margin-right to auto => This centers the div as it adds a margin from the left and the right.
    * App.js => Added Feed and Widget components.
    * Added a feed header with "Home" written in it.
    * Added .feed in css to add the flex with 0.4 in it. The total dlex should be 1(Sidebar = 0.3; Feed = 0.4; Widget = 0.3 totalling to 1).
    * Hid the scroll within the Feed component for all the browsers in the Feed.css file
    * Changed the color of the twitter icon to blue in the Sidebar.css file
    * Added basic widget component with the flex set to 0.3 to avoid the UI from breaking as the total flex should be 1.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
+import Feed from "./Feed";
 import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
 
 function App() {
   return (
     // BEM
     <div className="app">
-      <h1>Twitter clone</h1>
-
-      {/* Sidebar */}
       <Sidebar />
+
       {/* Feed */}
+      <Feed />
+
       {/* Widgets */}
+      <Widgets />
     </div>
   );
 }
---
+++ src/Feed.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Feed.css";
+
+function Feed() {
+  return (
+    <div className="feed">
+      {/* Header */}
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+      <h3>ASBSDSD</h3>
+
+      {/* Tweet box */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+    </div>
+  );
+}
+
+export default Feed;
