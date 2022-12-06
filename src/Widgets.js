bca31d1df8ca815b2632d5c518e0a1f0ad24407e
Nikhil Vijayan <nikhil321vijayan@gmail.com>
2022-12-06 17:03:57 -0500

    Started working on the Widgets component
    * Added a twitter search component
    * Added TwitterTweetEmbed in the What's happening widget section.
    * Added TwitterTimelineEmbed to embed my timeline in the Widgets section.
    * Added a TwitterShareButton element to create a pre-written tweet on click of the button.
    * Added necessary CSS for the Widgets.

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,8 +1,36 @@
 import React from "react";
 import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
-  return <div className="widgets">Widgets</div>;
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets_widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId="1596602316433784833" />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="nikhil_vijayan4"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={"https://twitter.com/nikhil_vijayan4"}
+          options={{ text: "#reactjs is awesome", via: "nikhil_vijayan4" }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Widgets;
